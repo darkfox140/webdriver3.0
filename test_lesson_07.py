8cfb6a715274e0d65c481db01889e6548a9947f9
fox140 <malcev223@gmail.com>
2022-02-24 15:00:17 +0300

    Исправление в цикле.

--- test_lesson_07.py
+++ test_lesson_07.py
@@ -14,6 +14,6 @@ def test_lesson_8(browser):
     browser.get("http://localhost/litecart/")
     browser.implicitly_wait(10)
     products = browser.find_elements(By.XPATH, "//*[@class='image-wrapper']")
-    for products in products:
-        stickers = products.find_elements(By.CSS_SELECTOR, ".sticker")
+    for product in products:
+        stickers = product.find_elements(By.CSS_SELECTOR, ".sticker")
         assert len(stickers) == 1
